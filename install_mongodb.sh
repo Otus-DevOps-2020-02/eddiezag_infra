07bf13742dd48843f815e4ffeca3cef91f8b443d
Eddie Zag <eddie.zag@gmail.com>
2020-03-25 13:44:04 +0200

    install_mongodb.sh added

---
+++ install_mongodb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt update && sudo apt install -y mongodb-org
+systemctl enable mongod && sudo systemctl start mongod
