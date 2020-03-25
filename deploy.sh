e57dc646ff2b1b51d540908af9d3ab990bffc3a6
Eddie Zag <38716641+eddiezag@users.noreply.github.com>
2020-03-25 12:31:39 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 #Downlading and install PR puma-server
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install && puma -d
+useradd appuser -s /bin/bash -m -G sudo
+echo "appuser:strongpasswdbestpract" | sudo chpasswd
+su -c 'cd /home/appuser && git clone -b monolith https://github.com/express42/reddit.git' appuser
+cd /home/appuser/reddit/ && sudo bundle install
+chown appuser:appuser -R /home/appuser
+systemctl enable puma.service && systemctl start puma.service
